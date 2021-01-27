8c4a4f5f86600265b2735f981a9d35f41cba9aa9
Alperen Efe URULU <05180000324@ogrenci.ege.edu.tr>
2021-01-27 03:10:41 +0300

    Add project files.

---
+++ ClassMethodDemo/CustomerMenager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class CustomerMenager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("New customer added: " + customer.ID + " - " + customer.Name);
+        }
+
+        public void Remove(Customer customer)
+        {
+            Console.WriteLine("The customer removed: " + customer.ID + " - " + customer.Name);
+        }
+
+        public void Listing(Customer customer)
+        {
+            Console.WriteLine("Customer ID: " + customer.ID + 
+                "\nCustomer Name: " + customer.Name + 
+                "\nCustomer Surname: " + customer.Surname + 
+                "\nCustomer Credit Type: " + customer.CreditType + 
+                "\nCustomer Interest Rate: " + "%" + customer.InterestRate);
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.ID =1;
+            customer1.Name = "John";
+            customer1.Surname = "Garcia";
+            customer1.CreditType = "Taşıt Kredisi";
+            customer1.InterestRate = 12;
+
+            Customer customer2 = new Customer();
+            customer2.ID = 2;
+            customer2.Name = "Linda";
+            customer2.Surname = "Wilson";
+            customer2.CreditType = "İhtiyaç Kredisi"; 
+            customer2.InterestRate = 23;
+
+            Customer customer3 = new Customer()
+            {
+                ID = 3,
+                Name = "James",
+                Surname = "Smith",
+                CreditType = "Konut Kredisi",
+                InterestRate = 17.5
+            };
+
+            Customer[] customers = new Customer[] { customer1, customer2, customer3 };
+
+            CustomerMenager customerMenager = new CustomerMenager();
+            customerMenager.Add(customer1);
+            customerMenager.Add(customer3);
+
+            customerMenager.Listing(customer2);
+
+            customerMenager.Remove(customer2);
+            customerMenager.Remove(customer3);
+        }
+    }
+}
